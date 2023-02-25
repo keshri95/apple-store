8c46cc7c339f5c74edab25406cea80bfcd88fd9b
Ashish Keshri <keshri.ashish1@gmail.com>
2023-02-25 21:58:03 +0530

    header

---
+++ components/Header.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { BsSearch } from "react-icons/bs";
+import { AiOutlineShopping } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+
+const Header = () => {
+
+  const session = false;
+
+  return (
+    <header className="sticky top-0 z-30 flex w-full items-center justify-between bg-[#E7ECEE] p-4">
+      <div className="flex items-center justify-center md:w-1/5">
+        <Link href="/">
+          <div className="relative h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100">
+            <Image
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/1667px-Apple_logo_black.svg.png"
+              alt="img"
+              fill
+              objectFit="contain"
+            />
+          </div>
+        </Link>
+      </div>
+      <div className="hidden flex-1 items-center justify-center space-x-8 md:flex">
+        <a className="headerLink">Products</a>
+        <a className="headerLink">Explore</a>
+        <a className="headerLink">Support</a>
+        <a className="headerLink">Business</a>
+      </div>
+
+      <div className="flex items-center justify-center gap-x-4 md:w-1/5">
+
+        <BsSearch className="headerIcon" />
+
+        <Link href="/checkout">
+          <div className="relative cursor-pointer">
+            <span className="absolute -right-1 -top-1 z-50 flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500">
+              5
+            </span>
+
+            <AiOutlineShopping className="" />
+          </div>
+        </Link>
+
+        {
+          session  ? (
+            <Image src="https://images.unsplash.com/photo-1677297680113-d508d9d01eee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+            alt=""
+            className="cursor-pointer rounded-full"
+            width={34}
+            height={34}
+            // onClick={() => signOut()}
+            />
+          ) : (
+            <AiOutlineUser
+              className="headerIcon"
+              //  onClick={() => signIn()}
+            />
+          )
+        }
+
+      </div>
+
+    </header>
+  );
+};
+
+export default Header;
