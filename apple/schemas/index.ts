43093062856e8ddaf6410f432b2fc054a4c5fe52
Ashish Keshri <keshri.ashish1@gmail.com>
2023-03-05 22:26:49 +0530

    sanity-package

--- apple/schemas/index.ts
+++ apple/schemas/index.ts
@@ -2,11 +2,12 @@ import category from './category'
 import product from './product'
 import {user, account} from 'next-auth-sanity/schemas'
 import {createSchema} from 'sanity'
+
+
 export const schemaTypes = [product, category, user, account]
 
 export default createSchema({
   name: 'default',
-    types: schemaTypes.concat([
-        product, category
-    ]),
+    types: schemaTypes
+
 })
